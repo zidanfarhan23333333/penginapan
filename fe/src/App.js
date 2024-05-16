9498f559e6931b3b49811b7be638c3a9e455663e
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2024-05-17 00:32:58 +0700

    fix routingan

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,16 +1,17 @@
 import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="flex w-full h-screen">
-      <div className="w-full flex items-center justify-center lg:w-1/2">
-        <Login />
-      </div>
-      <div className="hiden relative lg:flex items-center justify-center h-full w-1/2 bg-gray-200">
-        <div className="w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-400 rounded-full animate-bounce" />
-        <div className="w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-lg" />
-      </div>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
