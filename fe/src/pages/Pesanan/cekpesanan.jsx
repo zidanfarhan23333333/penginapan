7066a4890afa8b28e3c7852a572e4e62fabdc4ff
ferrykurniawann <ferrykurniawannnn@gmail.com>
2024-06-15 23:14:30 +0700

    cek pesanan

--- fe/src/pages/Pesanan/cekpesanan.jsx
+++ fe/src/pages/Pesanan/cekpesanan.jsx
@@ -7,14 +7,16 @@ const cekpesanan = () => {
 
       <div style={{width: 1700, height: 1276, position: 'relative', background: '#F2FAFD'}}>
       <div style={{width: 1520, height: 1077, left: 55, top: 212, position: 'absolute', background: 'white', borderRadius: 10}} />
-      <div style={{width: 1520, height: 93, left: 55, top: 99, position: 'absolute', background: 'white', borderRadius: 10}} />      
-      <div style={{left: 99, top: 0, position: 'absolute', color: 'black', fontSize: 32, fontFamily: 'Inter', fontWeight: '700', lineHeight: 24, wordWrap: 'break-word'}}>Pesanan anda</div>
+      <div style={{width: 1520, height: 93, left: 55, top: 99, position: 'absolute', background: 'white', borderRadius: 10, fontSize: 32, fontFamily: 'Inter', fontWeight: '700', lineHeight: 3,}}>Pesanan anda</div>
       <Link
           to="/accommodations"
           className="rounded-full bg-blue-500 text-white py-2 px-4 font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 hover:bg-blue-600 transition duration-300"
           style={{ zIndex: 10 }}
         >Booking
         </Link>
+        <li>
+            <Link to="/accommodations">Akomodasi</Link>
+          </li>
     </div>
     </div>
   );
